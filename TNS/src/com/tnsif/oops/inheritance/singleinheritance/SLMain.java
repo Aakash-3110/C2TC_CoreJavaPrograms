b18d24fe5db3421957370a93beb2f2cafb781595
bhara <bhara@Lisa>
2025-12-29 07:37:14 +0530

    inheritance,exceptional handling,userdefined input

---
+++ TNS/src/com/tnsif/oops/inheritance/singleinheritance/SLMain.java
@@ -0,0 +1,10 @@
+package com.tnsif.oops.inheritance.singleinheritance;
+
+public class SLMain {
+
+	public static void main(String[] args) {
+		Student student = new Student("Aakash", "1800180080", "Chennai", 9874561230L, 02, "MCE College");
+		System.out.println(student);
+	}
+
+}
